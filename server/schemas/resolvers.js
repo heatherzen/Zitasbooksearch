f54019ae7cc568e53f263d8b1dd681dd3a644ff5
heatherzen <heather.zen@gmail.com>
2020-11-08 22:13:17 -0700

    added mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,12 +1,50 @@
 const { User, Book } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
 
 const resolvers = {
     Query: {
-      thoughts: async (parent, { username }) => {
-          const params = username ? { username } : {};
-        return Book.find(params).sort({ createdAt: -1 });
-      }
-    }
-  };
+        books: async (parent, { username }) => {
+            const params = username ? { username } : {};
+            return Book.find(params).sort({ createdAt: -1 });
+        },
+        book: async(parent, { _id }) => {
+            return Book.findOne({ _id });
+        },
+        // get all users
+        users: async () => {
+            return User.find()
+                .select('-__v -password')
+                .populate('books');
 
-  module.exports = resolvers;
+        },
+        // get a user by username
+        user: async (parent, { username }) => {
+            return User.findOne({ username })
+                .select('-__v -password')
+                .populate('books');
+        },
+    },
+    Mutation: {
+        addUser: async () => {
+            const user = await User.create(args);
+            return user;
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
+          
+            const correctPw = await user.isCorrectPassword(password);
+          
+            if (!correctPw) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
+          
+            return user;
+        }
+    }
+};
+
+module.exports = resolvers;
